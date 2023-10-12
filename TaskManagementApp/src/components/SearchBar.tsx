b711eab867be62fa01efe5d5f5eff1d3415429c0
Alex Ogorek <alex@alexogorek.com>
2023-10-12 15:17:17 -0700

    search component or whatever

---
+++ TaskManagementApp/src/components/SearchBar.tsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import { View, TextInput, Button, StyleSheet } from 'react-native';
+
+interface SearchBarProps {
+    onSearch: (query: string) => void;
+}
+
+const SearchBar: React.FC<SearchBarProps> = ({ onSearch }) => {
+    const [searchQuery, setSearchQuery] = useState<string>('');
+
+    const handleSearch = () => {
+        if (searchQuery.trim() !== '') {
+            onSearch(searchQuery);
+        }
+    };
+
+    return (
+        <View className="flex-row p-8 bg-gray-200">
+            <TextInput
+                className="flex-1 bg-gray-300"
+                placeholder="Search..."
+                value={searchQuery}
+                onChangeText={(text) => setSearchQuery(text)}
+            />
+            <Button title="Search" onPress={handleSearch} />
+        </View>
+    );
+};
+
+// const styles = StyleSheet.create({
+//     container: {
+//         flexDirection: 'row',
+//         alignItems: 'center',
+//     },
+//     input: {
+//         flex: 1,
+//         padding: 10,
+//         borderBottomWidth: 1,
+//         marginRight: 10,
+//     },
+// });
+
+export default SearchBar;
--- TaskManagementApp/src/templates/Welcome.tsx
+++ TaskManagementApp/src/templates/Welcome.tsx
@@ -1,7 +1,14 @@
+import SearchBar from '@/components/SearchBar';
 import { Text, View } from 'react-native';
+
+const handleSearch = (searchQuery: string) => {
+  // Perform a search with the searchQuery
+  console.log('Searching for:', searchQuery);
+};
 
 const Welcome = () => (
   <View className="flex-1 items-center justify-center">
+    <SearchBar onSearch={handleSearch} />
     <Text className="text-blue-500">Open up App/ to start working on your app!</Text>
   </View>
 );
